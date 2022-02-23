b495fe0967b6c8fbf6ae8dd3ec58c330b16a8dff
SoftDevPinto <100211207+SoftDevPinto@users.noreply.github.com>
2022-02-23 15:27:42 -0330

    Add files via upload

---
+++ Grades - 2022/Grades.py
@@ -0,0 +1,16 @@
+StudentID = "8345637"
+Grade = 30
+
+if Grade >= 90:
+    Letter = "A"
+elif 80 <= Grade <= 89:
+    Letter = "B"
+elif 70 <= Grade <=79:
+    Letter = "C"
+elif 60 <= Grade <= 69:
+    Letter = "D"
+else:
+    Letter = "F"
+
+print(Letter)
+
